4bc1c5b0a11889b1dae2d11e7548a8d7e18ad143
Lloric Mayuga Garcia <lloricode@gmail.com>
2026-07-15 11:42:45 +0800

    Update getAction method and add type annotations
    Refactor getAction method to return object type and add type annotations for generics.

--- src/Support/Config.php
+++ src/Support/Config.php
@@ -8,13 +8,25 @@ use Saloon\Barstool\Exceptions\InvalidActionClass;
 
 class Config
 {
-    public static function getAction(string $actionName, string $actionBaseClass): mixed
+    /**
+     * @template TAction of object
+     *
+     * @param  class-string<TAction>  $actionBaseClass
+     * @return TAction
+     */
+    public static function getAction(string $actionName, string $actionBaseClass): object
     {
         $actionClass = self::getActionClass($actionName, $actionBaseClass);
 
         return app($actionClass);
     }
 
+    /**
+     * @template TAction of object
+     *
+     * @param  class-string<TAction>  $actionBaseClass
+     * @return class-string<TAction>
+     */
     public static function getActionClass(string $actionName, string $actionBaseClass): string
     {
         $actionClass = config("barstool.actions.{$actionName}");
@@ -28,6 +40,14 @@ class Config
         return $actionClass;
     }
 
+    /**
+     * @template TAction of object
+     *
+     * @param  class-string<TAction>  $actionBaseClass
+     * @param  class-string  $actionClass
+     *
+     * @phpstan-assert class-string<TAction> $actionClass
+     */
     protected static function ensureValidActionClass(string $actionName, string $actionBaseClass, string $actionClass): void
     {
         if (! is_a($actionClass, $actionBaseClass, true)) {
